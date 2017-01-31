b304a098003a889789cb295aa1cb19daec616ff4
Mat Hunter <mathew.hunter@gmail.com>
2017-01-31 10:58:47 -0500

    Minor cleanup to front-end
    Minor cleanup to the front-end code.

--- static/main.js
+++ static/main.js
@@ -1,25 +1,27 @@
 $(() => {
 
+  var $captionLabel = $("h2[name='caption']");
   var $resultLabel = $("h1[name='result']");
   var $formulaField = $("input[name='formula']");
 
   $("form").on("submit", (event) => {
-
     event.preventDefault();
 
     var formula = $.trim($formulaField.val());
-    $resultLabel.text("Rolling...");
+
+    $captionLabel.text("Rolling " + formula + "...");
+    $resultLabel.text("?");
 
     var request = $.ajax({
       url: "/roll/" + formula,
       dataType: "json"
+    }).then((data) => {
+      $captionLabel.text("Rolled " + formula + ":");
+      $resultLabel.html(data.result);
+    }, (err) => {
+      $captionLabel.text("Error");
+      $resultLabel.text("!");
     });
 
-    request.done((data) => {
-      $resultLabel.html(data.result);
-    });
-    request.fail((err) => {
-      $resultLabel.text("Error");
-    });
   });
 });
